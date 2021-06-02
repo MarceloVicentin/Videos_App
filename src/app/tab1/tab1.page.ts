c9bdac1c0990ff6732b720b86e095585b6308d6c
marcelo.vicentin@etec.sp.gov.br <marcelo.vicentin@etec.sp.gov.br>
2021-06-02 03:15:33 -0300

    Adicionado mais 3 filmes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -20,7 +20,8 @@ export class Tab1Page {
       duracao: '1h 50m',
       classificacao: 76,
       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ijvC2w2yANsfgLT3LMu2zFr0fxh.jpg',
-      generos: ['Ação', 'Fantasia', 'Aventura']
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat'
    },
 
    {
@@ -29,19 +30,39 @@ export class Tab1Page {
      duracao: '4h 2m',
      classificacao: 85,
      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
-     generos: ['Ação', 'Fantasia', 'Aventura']
+     generos: ['Ação', 'Fantasia', 'Aventura'],
+     pagina: '/liga-justica'
    },
 
    {
-    nome: 'NOVO FILME ADICIONADO',
-    lancamento: '18/03/2021',
-    duracao: '4h 2m',
-    classificacao: 85,
-    cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
-    generos: ['Ação', 'Fantasia', 'Aventura']
+    nome: 'Sem Remorso (2021) ',
+    lancamento: '130/04/2021',
+    duracao: '1h 50m',
+    classificacao: 72,
+    cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/uHEZ4ZMziIjlAgCTQAEh9ROvtj0.jpg',
+    generos: ['Ação', 'Thriller', 'Guerra'],
+   // pagina: '/liga-justica'
+  },
+
+  {
+    nome: 'Godzilla vs. Kong (2021) ',
+    lancamento: '01/04/2021',
+    duracao: '1h 53m',
+    classificacao: 81,
+    cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wllzjZxg4ynlAm5xmOICJ2uHOPJ.jpg',
+    generos: ['Ação','Aventura','Ficção Científica'],
+    //pagina: '/liga-justica'
+  },
+
+  {
+    nome: 'Na Mira do Perigo (2021)',
+    lancamento: '18/02/2021',
+    duracao: '1h 48m',
+    classificacao: 74,
+    cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wAJniojhHa4oVKa9AxdNwvpmS74.jpg',
+    generos: ['Ação', 'Thriller', 'Crime' ],
+    //pagina: '/liga-justica'
   }
-
-
 
 
   ];
